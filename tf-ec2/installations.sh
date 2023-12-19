df5cb11255c2e7b43b5680356ed8517d13eb300c
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2023-12-19 17:36:00 +0530

    Update installations.sh

--- tf-ec2/installations.sh
+++ tf-ec2/installations.sh
@@ -21,9 +21,10 @@ Content-Disposition: attachment; filename="userdata.txt"
 #!/bin/bash 
 sudo yum update -y
 sudo yum upgrade -y
+sudo yum install java -y
 sudo yum install docker -y
 sudo service docker start
 sudo usermod -aG docker ec2-user
 sudo setfacl -m user:ec2-user:rw /var/run/docker.sock
 sudo docker pull nginx
-sudo docker run -d -p 80:80 ng
+sudo docker run -d -p 80:80 nginx
