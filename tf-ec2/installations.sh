ba0748e3849b7ff851f805c08ecb48f1070c482b
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2023-12-22 19:24:57 +0530

    Update installations.sh

--- tf-ec2/installations.sh
+++ tf-ec2/installations.sh
@@ -12,13 +12,14 @@ Content-Type: text/x-shellscript; charset="us-ascii"
 MIME-Version: 1.0
 Content-Transfer-Encoding: 7bit
 
+######################  Update Linux packages and Install Java ######################
 #!/bin/bash 
-
 yum update -y
 yum upgrade -y
 yum install java-17-amazon-corretto-headless -y
 
 ######################  Install Docker and start docker service ######################
+
 yum install docker -y
 service docker start
 systemctl enable docker
@@ -27,8 +28,6 @@ setfacl -m user:ec2-user:rw /var/run/docker.sock
 
 ########################## Download and Install Apache Maven on Amazon Linux 2 ###################
 
-# script to install maven
-#!/bin/bash 
 set -e
 mvn_version=${mvn_version:-3.9.4}
 url="https://dlcdn.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
@@ -41,7 +40,6 @@ export M2_HOME=${install_dir}
 export M2=${install_dir}/bin
 export PATH=${install_dir}/bin:$PATH
 EOF
-
 source /etc/profile.d/maven.sh
 echo maven installed to ${install_dir}
 printf "\n\nTo get mvn in your path, open a new shell or execute: source /etc/profile.d/maven.sh\n"
@@ -58,12 +56,10 @@ curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-kubectl version --short --client
 
 ################### Install  Kustomize CLI tool on EC2 #############################
 curl --silent --location --remote-name \
   "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.2.3/kustomize_kustomize.v3.2.3_linux_amd64" && \
   chmod a+x kustomize_kustomize.v3.2.3_linux_amd64 && \
   sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
-
-kustomize version
+--//--
