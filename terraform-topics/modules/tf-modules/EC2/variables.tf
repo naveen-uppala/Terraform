410d712b52501083d7e58ab483e3e0f7cfc428d6
unaveengoud <unaveengoud.006@gmail.com>
2024-02-18 17:05:27 +0530

    message

--- terraform-topics/modules/tf-modules/EC2/variables.tf
+++ terraform-topics/modules/tf-modules/EC2/variables.tf
@@ -1,11 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
 variable "ami_id" {
   default = "ami-0e731c8a588258d0d"
 }
 
 variable "instance_type" {
   default = "t2.micro"
-}
-
-variable "aws_region" {
-  default = "us-east-1"
 }
--- terraform-topics/modules/us-east-2/variables.tf
+++ terraform-topics/modules/us-east-2/variables.tf
@@ -1,11 +1,11 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
 variable "ami_id" {
   default = "ami-0c20d88b0021158c6"
 }
 
 variable "instance_type" {
   default = "t2.medium"
-}
-
-variable "aws_region" {
-  default = "us-east-2"
 }
