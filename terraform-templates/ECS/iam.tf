7bb152375db8592d0ba3794907ab07b88f7bd8e6
SandeepKomal <sandeepkomalp@gmail.com>
2024-03-11 13:29:17 +0530

    ECS

---
+++ terraform-templates/ECS/iam.tf
@@ -0,0 +1,72 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "role-name"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+resource "aws_iam_role" "ecs_task_role" {
+  name = "role-name-task"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+resource "aws_iam_role_policy_attachment" "task_s3" {
+  role       = "${aws_iam_role.ecs_task_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+data "aws_iam_policy_document" "ecs_auto_scale_role" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["application-autoscaling.amazonaws.com"]
+    }
+  }
+}
+
+# ECS auto scale role
+resource "aws_iam_role" "ecs_auto_scale_role" {
+  name               = var.ecs_auto_scale_role_name
+  assume_role_policy = data.aws_iam_policy_document.ecs_auto_scale_role.json
+}
+
+# ECS auto scale role policy attachment
+resource "aws_iam_role_policy_attachment" "ecs_auto_scale_role" {
+  role       = aws_iam_role.ecs_auto_scale_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+}
---
+++ terraform-templates/ECS/provider.tf
@@ -0,0 +1,6 @@
+# Specify the provider and access details
+provider "aws" {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region     = var.aws_region
+}
